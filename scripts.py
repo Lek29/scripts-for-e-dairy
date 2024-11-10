479e42b8983d1447e3359c14557b024a5e086d71
Lek29 <ligioner29@mail.ru>
2024-11-11 06:43:03 +0800

    исправил ошибки в файле scripts.py

--- scripts.py
+++ scripts.py
@@ -3,39 +3,57 @@ from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 from datacenter.models import Schoolkid, Mark, Chastisement, Subject, Lesson, Commendation
 from random import choice
 
+
+def get_schoolkid(full_name):
+    try:
+        return Schoolkid.objects.get(full_name=full_name)
+    except ObjectDoesNotExist:
+        raise ValueError(f"Ученик с ФИО '{full_name}' не найден.")
+    except MultipleObjectsReturned:
+        raise ValueError(f"Найдено несколько учеников с ФИО '{full_name}'. Уточните ФИО.")
+
+
 def fix_marks(name):
-    student_name = Schoolkid.objects.filter(full_name__contains=name).first()
-    if not student_name:
-        print(f'Ученик с именем {name} не найден')
+    try:
+        student_name = get_schoolkid(name)
+    except ValueError as e:
+        print(e)
         return
+
     bad_marks = Mark.objects.filter(schoolkid_id=student_name.id, points__in=[2, 3])
     if not bad_marks.exists():
         print(f'У ученика {name} нет плохих оценок')
         return
-    for mark in bad_marks:
-        mark.points = choice([4, 5])
-        mark.save()
+
+    bad_marks.update(points=choice([4, 5]))
     print(f'Оценки ученика {name} успешно исправлены!')
 
 
 def remove_chastisements(schoolkid):
-    student_name = Schoolkid.objects.filter(full_name__contains=schoolkid)[0]
-    student_chastisement = Chastisement.objects.filter(schoolkid=student_name)
-    for chastisement in student_chastisement:
-        chastisement.delete()
+    try:
+        student = get_schoolkid(schoolkid)
+    except ValueError as e:
+        print(e)
+        return
+
+    Chastisement.objects.filter(schoolkid=student).delete()
 
 
 def create_commendation(full_name, subject_title):
     try:
-        schoolkid = Schoolkid.objects.get(full_name=full_name)
-    except ObjectDoesNotExist:
-        print(f"Ученик с ФИО '{full_name}' не найден.")
-        return
-    except MultipleObjectsReturned:
-        print(f"Найдено несколько учеников с ФИО '{full_name}'. Уточните ФИО.")
+        schoolkid = get_schoolkid(full_name)
+    except ValueError as e:
+        print(e)
         return
 
-    subject = Subject.objects.filter(title=subject_title, year_of_study=schoolkid.year_of_study).first()
+    try:
+        subject = Subject.objects.get(title=subject_title, year_of_study=schoolkid.year_of_study)
+    except Subject.DoesNotExist:
+        print(f"Предмет с названием '{subject_title}' не найден.")
+        return
+    except Subject.MultipleObjectsReturned:
+        print(f"Найдено несколько предметов с названием '{subject_title}'. Уточните название.")
+        return
 
     lessons = Lesson.objects.filter(
         year_of_study=schoolkid.year_of_study,
